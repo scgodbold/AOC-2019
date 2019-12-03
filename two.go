44e32e216e2bd97653e71011fa09633714dc6824
Scott Godbold <scgodbold@gmail.com>
2019-12-02 23:05:23 -0500

    Got day 2 working for both segments

--- two.go
+++ two.go
@@ -7,34 +7,127 @@ import (
 )
 
 type Program struct {
-	Instructions []int
-	Pointer      int
+	DefaultMemory []int
+	Memory        []int
+	Pointer       int
 }
 
 func NewProgram(input []string) (*Program, error) {
 	var prog Program
 	for _, line := range input {
-
 		for _, val := range strings.Split(line, ",") {
 			cleaned, err := strconv.Atoi(val)
 			if err != nil {
 				return nil, err
 			}
-			prog.Instructions = append(prog.Instructions, cleaned)
+			prog.DefaultMemory = append(prog.DefaultMemory, cleaned)
 		}
 	}
-	prog.Pointer = 0
+	prog.Reset()
 	return &prog, nil
 }
 
-func (p *Program) opCodeAdd() {
+func (p *Program) Reset() {
+	p.Memory = make([]int, len(p.DefaultMemory))
+	copy(p.Memory, p.DefaultMemory)
+	p.Pointer = 0
+}
+
+func (p *Program) Run() {
+	for {
+		if len(p.Memory) < p.Pointer {
+			break
+		}
+		switch p.Memory[p.Pointer] {
+		case 1:
+			p.opAdd()
+		case 2:
+			p.opMult()
+		case 99:
+			break
+		default:
+			break
+		}
+		p.Pointer += 4
+	}
+}
+
+func (p *Program) opMult() {
+	xVal := p.Memory[p.Memory[p.Pointer+1]]
+	yVal := p.Memory[p.Memory[p.Pointer+2]]
+	storePos := p.Memory[p.Pointer+3]
+	p.Memory[storePos] = xVal * yVal
+}
+
+func (p *Program) opAdd() {
+	xVal := p.Memory[p.Memory[p.Pointer+1]]
+	yVal := p.Memory[p.Memory[p.Pointer+2]]
+	storePos := p.Memory[p.Pointer+3]
+	p.Memory[storePos] = xVal + yVal
+}
+
+func DayTwoPartOne(prog *Program) {
+	// Alter Noun and Verb state of program
+	prog.Memory[1] = 12
+	prog.Memory[2] = 2
+
+	// Execute Program
+	prog.Run()
+}
+
+func DayTwoPartTwo(prog *Program) (int, int) {
+	const (
+		wanted    = 19690720
+		nounBound = 99
+		verbBound = 99
+	)
+	var (
+		noun int
+		verb int
+	)
+
+	found := false
+	for i := 0; i <= nounBound; i++ {
+		for j := 0; j <= verbBound; j++ {
+			prog.Reset()
+			prog.Memory[1] = i
+			prog.Memory[2] = j
+			prog.Run()
+
+			if prog.Memory[0] == wanted {
+				found = true
+				noun = i
+				verb = j
+				break
+			}
+
+			// Since we can only make values larger if the value
+			// was larger then what we want, this isnt working move along
+			if prog.Memory[0] > wanted {
+				break
+			}
+		}
+		if found {
+			break
+		}
+	}
+
+	return noun, verb
 }
 
 func DayTwo(input []string) {
 	prog, err := NewProgram(input)
 	if err != nil {
-		fmt.Printf("Failed to process program")
+		fmt.Printf("Failed to parse program for Day2 Part1: %v\n", err)
 		return
 	}
-	fmt.Println(prog.Instructions)
+
+	// Part one solution
+	DayTwoPartOne(prog)
+	fmt.Printf("Return value of the program upon execution is: %v\n", prog.Memory[0])
+
+	prog.Reset()
+	// Part two solution
+	noun, verb := DayTwoPartTwo(prog)
+	fmt.Printf("Noun (%v) and Verb (%v) provide expected output", noun, verb)
 }
--- two_test.go
+++ two_test.go
@@ -4,16 +4,57 @@ import (
 	"testing"
 )
 
-func TestNewProgram(t *testing.T) {
-	var input []string
-	input = append(input, "1")
-
-	prog, err := NewProgram(input)
-	if err != nil {
-		t.Errorf("Return for NewProgram was an error: %v", err)
+func progCompare(x []int, y []int) bool {
+	if len(x) != len(y) {
+		return false
 	}
 
-	if prog.Instructions[0] == 1 {
-		t.Errorf("Return value for NewProgram(\"1\") returned the wrong value: wanted 1, got %v", prog)
+	for i, v := range x {
+		if y[i] != v {
+			return false
+		}
+	}
+
+	return true
+}
+
+func TestProgRun(t *testing.T) {
+	tables := []struct {
+		input  []int
+		output []int
+	}{
+		{[]int{1, 0, 0, 0, 99}, []int{2, 0, 0, 0, 99}},
+		{[]int{2, 3, 0, 3, 99}, []int{2, 3, 0, 6, 99}},
+		{[]int{2, 4, 4, 5, 99, 0}, []int{2, 4, 4, 5, 99, 9801}},
+		{[]int{1, 1, 1, 4, 99, 5, 6, 0, 99}, []int{30, 1, 1, 4, 2, 5, 6, 0, 99}},
+	}
+	for _, val := range tables {
+		input_prog := make([]int, len(val.input))
+		copy(input_prog, val.input)
+		prog := Program{val.input, input_prog, 0}
+		prog.Run()
+		if !progCompare(prog.Memory, val.output) {
+			t.Errorf("Program (%v) did not execute as expected: got: %v, wanted: %v", val.input, prog.Memory, val.output)
+		}
+	}
+}
+
+func TestProgReset(t *testing.T) {
+	input := []int{1, 0, 0, 0, 99}
+
+	// Use distinct copies so input maynot be modified
+	copyOne := make([]int, len(input))
+	copyTwo := make([]int, len(input))
+	copy(copyOne, input)
+	copy(copyTwo, input)
+	prog := Program{copyOne, copyTwo, 0}
+
+	// Execute + Reset
+	prog.Run()
+	prog.Reset()
+
+	// Validate that Memory is back to starting position
+	if !progCompare(prog.Memory, input) {
+		t.Errorf("Program (%v) did not reset correctly: got: %v, wanted: %v", input, prog.Memory, input)
 	}
 }
